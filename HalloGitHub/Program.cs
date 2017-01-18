b45ecfca7f99d2d85686903e35ba035398f8da7e
KaivdP <kai@kaivdp.de>
2017-01-19 00:05:06 +0100

    ASDSAD
    SADSAD

--- HalloGitHub/Program.cs
+++ HalloGitHub/Program.cs
@@ -85,6 +85,7 @@ namespace HalloGitHub
             Console.WriteLine(" - Klatoo");
             Console.WriteLine(" - Lopi-Of");//zweiter PULL REQUEST
             Console.WriteLine(" - CeeeJay");
+            Console.WriteLine(" - Kaivdp"); //Neuer PULL
             // Kopiere die vorherige Zeile und verwende deinen Namen anstelle von LMStudent.
 
             Console.WriteLine();
